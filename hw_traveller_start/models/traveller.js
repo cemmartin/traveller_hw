da57d14ab485793680d3ffaa7a5c6c948833f42d
Claire Martin <claire.e.martin15@gmail.com>
2023-09-27 21:37:58 +0100

    initial upload

---
+++ hw_traveller_start/models/traveller.js
@@ -0,0 +1,32 @@
+class Traveller {
+  constructor(journeys) {
+    this.journeys = journeys;
+  }
+
+  getJourneyStartLocations() {
+// writing something that will give us the location of each --> mao
+  }
+
+  getJourneyEndLocations() {
+
+  }
+
+  getJourneysByTransport(transport) {
+
+  }
+
+  getJourneysByMinDistance(minDistance) {
+
+  }
+
+  calculateTotalDistanceTravelled() {
+
+  }
+
+  getUniqueModesOfTransport() {
+
+  }
+}
+
+
+module.exports = Traveller;
